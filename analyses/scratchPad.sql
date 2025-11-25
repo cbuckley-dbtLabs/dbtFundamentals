8bfa8360ad3fd240049d6541fa6d1da703b38ff7
Cormac Buckley <cormac.buckley@fishtownanalytics.com>
2025-11-25 15:11:52 +0000

    updates

--- analyses/scratchPad.sql
+++ analyses/scratchPad.sql
@@ -1,11 +1,3 @@
 -- models/country_codes_model.sql
 
-WITH country_codes AS (
-    SELECT *
-    FROM {{ ref('countries') }}
-)
-
-SELECT
-    Country_Name,
-    ISO
-FROM country_codes
+select * from {{ref('SandboxIntro', 'fct_order_items')}}
