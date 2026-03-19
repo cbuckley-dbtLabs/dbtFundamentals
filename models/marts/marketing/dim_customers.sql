c2b45105693e35b5a09588fc03645727853ef5d2
Cormac Buckley <cormac.buckley@dbtlabs.com>
2026-03-19 10:11:20 +0000

    Fixed time spines

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,68 +1,74 @@
 -- customers.sql
 with
 
-    customers as (select * from {{ ref("stg_jaffle_shop__customers") }}),
+customers as (select * from {{ ref("stg_jaffle_shop__customers") }}),
 
-    orders as (select * from {{ ref("fct_orders") }}),
+orders as (select * from {{ ref("fct_orders") }}),
 
-    order_history as (select * from {{ ref("int_customer_order_history") }}),
+order_history as (select * from {{ ref("int_customer_order_history") }}),
 
-    customer_orders as (
+customer_orders as (
 
-        select
-            customer_id,
-            min(order_date) as first_order_date,
-            max(order_date) as most_recent_order_date,
-            count(order_id) as number_of_orders,
-            sum(amount) as lifetime_value
+    select
+        customer_id,
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
 
-        from orders
+    from orders
 
-        group by 1
+    group by 1
 
-    ),
+),
 
-    order_gaps as (
+order_gaps as (
 
-        select
-            customer_id,
-            round(avg(days_since_previous_order), 1) as avg_days_between_orders
+    select
+        customer_id,
+        round(avg(days_since_previous_order), 1) as avg_days_between_orders
 
-        from order_history
+    from order_history
 
-        where days_since_previous_order is not null
+    where days_since_previous_order is not null
 
-        group by 1
+    group by 1
 
-    ),
+),
 
-    customers_enriched as (
+customers_enriched as (
 
-        select
-            customers.customer_id,
-            customers.first_name,
-            customers.last_name,
-            customer_orders.first_order_date,
-            customer_orders.most_recent_order_date,
-            coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-            customer_orders.lifetime_value,
-            order_gaps.avg_days_between_orders,
-            datediff(
-                'day', customer_orders.most_recent_order_date, current_date
-            ) as days_since_last_order,
-            case
-                when coalesce(customer_orders.number_of_orders, 0) = 0 then 'No Orders'
-                when coalesce(customer_orders.number_of_orders, 0) = 1 then 'New'
-                when coalesce(customer_orders.number_of_orders, 0) <= 4 then 'Repeat'
-                else 'Loyal'
-            end as customer_segment
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        customer_orders.lifetime_value,
+        order_gaps.avg_days_between_orders,
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        datediff(
+            'day', customer_orders.most_recent_order_date, current_date
+        ) as days_since_last_order,
+        case
+            when
+                coalesce(customer_orders.number_of_orders, 0) = 0
+                then 'No Orders'
+            when coalesce(customer_orders.number_of_orders, 0) = 1 then 'New'
+            when
+                coalesce(customer_orders.number_of_orders, 0) <= 4
+                then 'Repeat'
+            else 'Loyal'
+        end as customer_segment
 
-        from customers
+    from customers
 
-        left join customer_orders on customers.customer_id = customer_orders.customer_id
-        left join order_gaps on customers.customer_id = order_gaps.customer_id
+    left join
+        customer_orders
+        on customers.customer_id = customer_orders.customer_id
+    left join order_gaps on customers.customer_id = order_gaps.customer_id
 
-    )
+)
 
 select *
 from customers_enriched
