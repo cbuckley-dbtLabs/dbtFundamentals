a994c0635db1b2fb7d61090592e42192986748a3
cbuckley-dbtLabs <cormac.buckley@dbtlabs.com>
2026-04-07 15:17:07 +0100

    Auto commit for Studio handoff

---
+++ models/canvas/Canvas_Model.sql
@@ -0,0 +1,73 @@
+WITH o AS (
+  SELECT
+    *
+  FROM {{ ref('fct_orders') }}
+), c AS (
+  SELECT
+    *
+  FROM {{ ref('dim_customers') }}
+), rename_1 AS (
+  SELECT
+    CUSTOMER_ID AS O_CUSTOMER_ID,
+    *
+    EXCLUDE (CUSTOMER_ID)
+  FROM o
+), rename_2 AS (
+  SELECT
+    CUSTOMER_ID AS C_CUSTOMER_ID,
+    *
+    EXCLUDE (CUSTOMER_ID)
+  FROM c
+), join_1 AS (
+  SELECT
+    *
+  FROM rename_1
+  JOIN rename_2
+    ON rename_1.O_CUSTOMER_ID = rename_2.C_CUSTOMER_ID
+), aggregate_1 AS (
+  SELECT
+    O_CUSTOMER_ID,
+    FIRST_NAME,
+    LAST_NAME,
+    MIN(ORDER_DATE) AS FIRST_ORDER_DATE,
+    MAX(ORDER_DATE) AS MOST_RECENT_ORDER_DATE,
+    COUNT(ORDER_ID) AS NUMBER_OF_ORDERS,
+    SUM(AMOUNT) AS TOTAL_SPENT,
+    SUM(GIFT_CARD_AMOUNT) AS TOTAL_GIFT_CARD_USED
+  FROM join_1
+  GROUP BY
+    O_CUSTOMER_ID,
+    FIRST_NAME,
+    LAST_NAME
+), rename_3 AS (
+  SELECT
+    O_CUSTOMER_ID AS CUSTOMER_ID,
+    FIRST_NAME,
+    LAST_NAME,
+    FIRST_ORDER_DATE,
+    MOST_RECENT_ORDER_DATE,
+    NUMBER_OF_ORDERS,
+    TOTAL_SPENT,
+    TOTAL_GIFT_CARD_USED
+  FROM aggregate_1
+), order_1 AS (
+  SELECT
+    *
+  FROM rename_3
+  ORDER BY
+    TOTAL_SPENT DESC
+), canvas_model_sql AS (
+  SELECT
+    CUSTOMER_ID,
+    FIRST_NAME,
+    LAST_NAME,
+    FIRST_ORDER_DATE,
+    MOST_RECENT_ORDER_DATE,
+    NUMBER_OF_ORDERS,
+    TOTAL_SPENT,
+    TOTAL_GIFT_CARD_USED
+  FROM order_1
+)
+SELECT
+  *
+FROM canvas_model_sql
